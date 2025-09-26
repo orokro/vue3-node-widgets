5e8799b64a45fab87672a7bd8d74487aa5d41513
Greg Miller <hypurban86@gmail.com>
2025-09-26 06:12:41 -0400

    Implements window manager for graphs

---
+++ src/dev_components/GraphWindow.vue
@@ -0,0 +1,46 @@
+<!--
+	GraphWindow.vue
+	---------------
+
+	For our vue-win-mgr system, this will be the window that loads the NWEditorGraph.vue component.
+-->
+<template>
+
+	<div class="graph-window">
+		<NWEditorGraph 
+			ref="myGraph" 
+			class="my-graph" 
+			:showDevErrors="true" 
+		/>
+	</div>
+
+</template>
+<script setup>
+
+// vue
+import { ref, onMounted } from 'vue';
+
+// our app
+import NWEditorGraph from '@Components/NWEditorGraph.vue';
+
+// ref to our graph, so we can expose to window for debugging
+const myGraph = ref(null);
+
+</script>
+<style lang="scss" scoped>
+
+	// container for the entire graph window
+	.graph-window {
+
+		// for debug
+		/* border: 2px solid red; */
+
+		// fill container
+		width: 100%;
+		height: 100%;
+		overflow: clip;
+		position: relative;
+
+	}// .graph-window
+
+</style>
